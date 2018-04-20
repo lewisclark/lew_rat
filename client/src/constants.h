b765533295cb6e96e624fe4292b2f7375b6529b6
Lewis <lewez@mail.com>
2018-04-20 15:46:56 +0100

    Specifiy listening ports for both client & server

--- client/src/constants.h
+++ client/src/constants.h
@@ -2,7 +2,8 @@
 #define CONSTANTS_H
 
 #define SERVER_ADDRESS "127.0.0.1"
-#define NET_PORT 16778
+#define SERVER_LISTENING_PORT 16778
+#define CLIENT_LISTENING_PORT 16779
 #define NET_BUFFER_SIZE 100000
 
 #define HEARTBEAT_FREQUENCY 5 // Delay (in seconds) between sending heartbeats
